21f31a75805da28bd72d6b97c4e93c4cb2190c96
judeadams21 <815386@mystma.org>
2021-01-26 20:01:26 +0000

    Unit 2 Test Ver. I

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f < 33:
+        return "solid"
+    elif 212 > f > 32:
+        return "liquid"
+    else:
+        return "gas"
+
+def isDozen(d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+
+def toGerman(x):
+    if x == "yes":
+        return "ja"
+    else:
+        return "nein"
+
+def stopLight(c):
+    if c == "green":
+        return "go"
+    elif c == "yellow":
+        return "slow"
+    else:
+        return "stop"
